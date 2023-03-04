6aa6fe5b52dd55f937554c8ca3cf19cbf4f02f04
Alaikum <carangiadamo@gmail.com>
2023-03-04 16:56:10 +0100

    add show, tags,visibility and comment

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -6,43 +6,56 @@ function listaFoto(){
     axios.get("http://localhost:8080/api/foto").then((res)=>{
         console.log(res.data)
         res.data.forEach(f => {
-            document.querySelector('#lista').innerHTML += `
-            <div class="col">
-					<div class="card ">
-						<img src="${f.url}" class="card-img-top" alt="...">
-						<div class="card-body">
-							<h5 class="card-title">${f.titolo}</h5>
-							<p class="card-text">
-								Descrizione: <br> <i>${f.descrizione}</i>
+			
+			if (f.visibile) {
+				
+				let textCat=""
+				if(f.categorie.length >0){
+					f.categorie.forEach(c => {
+						textCat+=c.nome+" "
+					});
+				}else{
+					textCat+="Nessuna categoria assegnata!" 
+				}
+				
+				
+				document.querySelector('#lista').innerHTML += `
+				<div class="col">
+				<div class="card ">
+				<img src="${f.url}" class="card-img-top" alt="...">
+				<div class="card-body">
+				<h5 class="card-title">${f.titolo}</h5>
+				<p class="card-text">
+				Descrizione: <br> <i>${f.descrizione}</i>
 							</p>
 							<p class="card-text">
-								Numero Commenti:  <i class="bg-info p-1">f.commenti.size</i>
+							Numero Commenti:  <i class="bg-info p-1">${f.commenti.length}</i>
 							</p>
 							<p class="card-text">
-								Categorie: <i >- </i> <i>Nessuna Categoria Salvata </i>
+							Categorie: <i >${textCat}</i>
+							
+							</p>
+							
 
 							</p>
-							<p class="card-text">
-								Visibilità: <i class="fa-solid fa-thumbs-up"
-									></i> <i class="fa-solid fa-thumbs-down"
-									></i>
-
-							</p>
-						</div>
-                        <div
+							</div>
+							<div
 							class="card-footer d-xl-flex justify-content-between  align-items-center">
 							<small class="text-muted">Tag: ${f.tag}</small>  <a
                             href="/frontend/show?id=${f.id}"><small class="text-muted">Dettaglio:
-								<i class="fa-solid fa-circle-info"></i></a></small>
+							<i class="fa-solid fa-circle-info"></i></a></small>
 							
-						</div>
-					</div>
-					
-				</div>
-            `
-            
-        });
-    }).catch((res)=>{
-        console.log("not Ok",res)
-    })
+							</div>
+							</div>
+							
+							</div>
+							`
+							
+						}else{
+							console.log("foto con id"+f.id+" non visibile")
+						}
+						});
+					}).catch((res)=>{
+						console.log("not Ok",res)
+					})
 }
